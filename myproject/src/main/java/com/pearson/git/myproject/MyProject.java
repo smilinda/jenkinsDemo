fadbb6dfde77823aed5ba7b45b94d0a164d903a1
Shiran Milinda <smilinda@localhost.localdomain>
2016-05-17 21:43:36 +0530

    Added myproject

---
+++ myproject/src/main/java/com/pearson/git/myproject/MyProject.java
@@ -0,0 +1,12 @@
+package com.pearson.git.myproject;
+
+import com.pearson.git.mysample.MySample;
+
+
+public class MyProject {
+	
+	public static void main(String[] args) {
+		System.out.println(MySample.getMySample());
+	}
+
+}
